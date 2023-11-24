bd32bb24fcf367a231f7af1f414836c658952d7a
ANDRES <anfevasa@gmail.com>
2023-11-24 16:44:53 -0500

    Agregando funcionalidad a Historial

--- front/src/view/components/consultarSaldoComponentes/Historial.jsx
+++ front/src/view/components/consultarSaldoComponentes/Historial.jsx
@@ -1,9 +1,17 @@
-function Historial(){
-    return(
-        <>
-            Historial
-        </>
-    )
+import MovimientoCard from "./MovimientoCard"
+import "./Historial.css"
+
+function Historial({cuentaActual}){
+
+    return(<section className="movimientos-container">
+        
+            {
+                cuentaActual.gastos.map((g)=>{
+                    return <MovimientoCard gastoId={g} numeroCuentaActual={cuentaActual.id} key={g.id} />
+                })
+
+            }
+    </section>)
 }
 
 export default Historial
---
+++ front/src/view/components/consultarSaldoComponentes/MovimientoCard.jsx
@@ -0,0 +1,92 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./MovimientoCard.css";
+
+function MovimientoCard({ gastoId, numeroCuentaActual }) {
+  const [gasto, setGasto] = useState();
+
+  const [tipoMovimiento, setTipoMovimiento] = useState();
+  const [entraDinero, setEntraDinero] = useState();
+  //   const [cuentaTransferencia, setCuentaTransferencia] = useState(undefined);
+
+  useEffect(() => {
+    const traerGasto = async () => {
+      let response = await axios.get(
+        `http://localhost:3000/gastos?id=${gastoId}`
+      );
+      setGasto(response?.data[0]);
+
+    //   if (gasto?.cuentaOrigen == numeroCuentaActual) {
+    //     if (gasto?.cuentaDestino == numeroCuentaActual) {
+    //       setTipoMovimiento("Deposito");
+    //       setEntraDinero(true);
+    //     } else {
+    //       //   let responseCuenta = await axios.get(
+    //       //     `http://localhost:3000/cuentas?id=${gasto?.cuentaDestino}`
+    //       //   );
+    //       //   setCuentaTransferencia(responseCuenta?.data[0]?.numeroCuenta);
+    //       setTipoMovimiento("Transferencia");
+    //       setEntraDinero(false);
+    //     }
+    //   } else {
+    //     // let responseCuenta = await axios.get(
+    //     //   `http://localhost:3000/cuentas?id=${gasto?.cuentaOrigen}`
+    //     // );
+    //     // setCuentaTransferencia(responseCuenta?.data[0]?.numeroCuenta);
+    //     setTipoMovimiento("Transferencia");
+    //     setEntraDinero(true);
+    //   }
+    };
+    traerGasto();
+  }, []);
+
+    useEffect(() => {
+      const traerCuenta = async () => {
+        if (gasto?.cuentaOrigen == numeroCuentaActual) {
+          if (gasto?.cuentaDestino == numeroCuentaActual) {
+            setTipoMovimiento("Deposito");
+            setEntraDinero(true);
+          } else {
+            // let responseCuenta = await axios.get(
+            //   `http://localhost:3000/cuentas?id=${gasto?.cuentaDestino}`
+            // );
+            // setCuentaTransferencia(responseCuenta?.data[0]?.numeroCuenta);
+            setTipoMovimiento("Transferencia");
+            setEntraDinero(false);
+          }
+        } else {
+        //   let responseCuenta = await axios.get(
+        //     `http://localhost:3000/cuentas?id=${gasto?.cuentaOrigen}`
+        //   );
+        //   setCuentaTransferencia(responseCuenta?.data[0]?.numeroCuenta);
+          setTipoMovimiento("Transferencia");
+          setEntraDinero(true);
+        }
+      };
+      traerCuenta();
+    }, [gasto]);
+
+  return (
+    <article className="card-movimiento-container">
+      <div>
+        <h4>{tipoMovimiento}</h4>
+        {tipoMovimiento == "Transferencia" ? (
+          !entraDinero ? (
+            <p>{"Hacia: N° " + gasto?.cuentaDestino + " id: " + gasto?.id}</p>
+          ) : (
+            <p>{"Desde: N° " + gasto?.cuentaOrigen + " id: " + gasto?.id}</p>
+          )
+        ) : (
+          <></>
+        )}
+      </div>
+      <p>{gasto?.fecha}</p>
+      <div>
+        <p className={entraDinero ? "entra" : "sale"}>{"$" + gasto?.valor.toLocaleString('es-CO')}</p>
+        <p>{gasto?.categoria}</p>
+      </div>
+    </article>
+  );
+}
+
+export default MovimientoCard;
