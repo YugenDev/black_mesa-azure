97e01348c2655dc467b7ef341bb86f535b7bfbeb
ANDRES <anfevasa@gmail.com>
2023-11-26 23:13:35 -0500

    Estilando portada

--- front/src/view/components/vistaVisitanteComponents/Portada.jsx
+++ front/src/view/components/vistaVisitanteComponents/Portada.jsx
@@ -1,21 +1,35 @@
-import logotipo from "../../../assets/images/logo-bmb.png"
-import "./Portada.css"
+import logotipo from "../../../assets/images/logo-bmb.png";
+import coin from "../../../assets/images/coin.png";
+// import model from "../../../assets/3d-model/mesh.obj"
+import "./Portada.css";
+
 function Portada() {
   return (
     <section className="portada">
-
-      <article className="contenedor-titulos">
-        <h1>Frase Importante</h1>
-        <h2>Aqui</h2>
-        <h3>Texto sobre la inteligencia artificial</h3>
-        <h3>Aqui, para organizar bien bonito</h3>
+      <div className="contenedor-titulos">
+        <div>
+          <h1>
+            LA TECNOLOG<strong>IA</strong>
+          </h1>
+          <h1>DEL FUTURO A TU SERVICIO</h1>
+        </div>
+        <div>
+          <h3>
+            Obtén hoy mismo tu <strong>asistente financiero personal</strong>,
+            impulsado por inteligencia artificial
+          </h3>
+          <h3>Siempre disponible, siempre listo para ayudarte.</h3>
+        </div>
         <button>Registrarse</button>
-      </article>
+      </div>
 
-        <article className="contenedor-logo">
-            <img src={logotipo} alt="logo" />
-        </article>
-      
+      <div className="contenedor-logo">
+        <img src={logotipo} alt="logo" className="logo-img"/>
+        <img src={coin} alt="coin" className="coin" />
+        <img src={coin} alt="coin" className="coin" />
+        <img src={coin} alt="coin" className="coin" />
+        <img src={coin} alt="coin" className="coin" />
+        </div>
     </section>
   );
 }
