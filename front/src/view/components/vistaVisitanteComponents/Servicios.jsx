655205dac9c0ba24601d44685d769c4ab99f0068
ANDRES <anfevasa@gmail.com>
2023-11-27 00:25:59 -0500

    estilando servicios

--- front/src/view/components/vistaVisitanteComponents/Servicios.jsx
+++ front/src/view/components/vistaVisitanteComponents/Servicios.jsx
@@ -1,33 +1,41 @@
 import "./Servicios.css";
 import billetera from "./imgsVistaVisitante/billetera.png";
-import tarjetas from "./imgsVistaVisitante/tarjetas-de-credito.png"
-import estadisticas from "./imgsVistaVisitante/estadisticas.png"
+import tarjetas from "./imgsVistaVisitante/tarjetas-de-credito.png";
+import estadisticas from "./imgsVistaVisitante/estadisticas.png";
 function Servicios() {
   return (
     <section className="contenedor-general-servicios">
       <article className="contenedor-titulo">
-        <h2>Nuestros Servicios</h2>
+        <h2>Nuestros servicios</h2>
       </article>
       <article className="contenedor-servicios">
-
         <div className="servicio">
           <img src={billetera} alt="billetera" />
           <h3>Cuenta de ahorros</h3>
-          <button>ver</button>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem illum suscipit temporibus.</p>
+          <button>Ir</button>
         </div>
 
         <div className="servicio">
           <img src={tarjetas} alt="billetera" />
           <h3>Tarjetas de credito</h3>
-          <button>ver</button>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem illum suscipit temporibus.</p>
+          <button>Ir</button>
         </div>
 
         <div className="servicio">
           <img src={estadisticas} alt="billetera" />
           <h3>Estadisticas de movimientos</h3>
-          <button>ver</button>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem illum suscipit temporibus.</p>
+          <button>Ir</button>
         </div>
 
+        <div className="servicio">
+          <img src={estadisticas} alt="billetera" />
+          <h3>Estadisticas de movimientos</h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem illum suscipit temporibus.</p>
+          <button>Ir</button>
+        </div>
       </article>
     </section>
   );
