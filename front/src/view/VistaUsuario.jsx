62b90cf476f1f019efc6a551a7bc1e4188a4d76e
ANDRES <anfevasa@gmail.com>
2023-11-29 13:41:03 -0500

    Agregando y estilando sección Tarjetas

--- front/src/view/VistaUsuario.jsx
+++ front/src/view/VistaUsuario.jsx
@@ -3,6 +3,7 @@ import ConsultarSaldo from "./sections/ConsultarSaldo";
 import Transferir from "./sections/Transferir";
 import Chatbot from './sections/Chatbot';
 import "./VistaUsuario.css"
+import Tarjetas from './sections/Tarjetas';
 
 function VistaUsuario ({currentUser}) {
   return (
@@ -10,6 +11,7 @@ function VistaUsuario ({currentUser}) {
         <Chatbot currentUser={currentUser} />
         <ConsultarSaldo currentUser={currentUser} />
         <Transferir currentUser={currentUser} />
+        <Tarjetas />
     </main>
   )
 }
