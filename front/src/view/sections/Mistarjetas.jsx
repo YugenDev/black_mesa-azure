7e590ec11d97b22ee8bebe69107b172005f17156
ANDRES <anfevasa@gmail.com>
2023-12-12 01:18:56 -0500

    mostrando mis tarjetas

--- front/src/view/sections/Mistarjetas.jsx
+++ front/src/view/sections/Mistarjetas.jsx
@@ -1,22 +1,71 @@
-import React, { useEffect } from 'react'
-import "./Mistarjetas.css"
+import React, { useEffect, useState } from "react";
+import chip from "./../../assets/images/chip.png";
+import visa from "./../../assets/images/visa.png";
+import "./Mistarjetas.css";
+import axios from "axios";
 
-const Mistarjetas = () => {
-  useEffect(()=>{
-
-  },[])
-
+const Mistarjetas = ({ currentUser, actualizar }) => {
+  const [tarjetas, setTarjetas] = useState([]);
+  useEffect(() => {
+    const traerTarjetas = async () => {
+      let response = await axios.get(`
+      http://localhost:3000/tarjeta?idUsuario=${currentUser.id}
+    `);
+      setTarjetas(response.data);
+    };
+    traerTarjetas();
+  }, [actualizar]);
 
   return (
-    <div className='mistarjetas'>
-        <div className='tarjeta'>
-            <p>tarjetas</p>
+    tarjetas?.length && (
+      <div className="MisTarjetas-Container">
+        <h2>Mis tarjetas</h2>
+        <div className="mistarjetas">
+          {tarjetas?.map((tarjeta) => {
+            return (
+              <article className="flip-card">
+                <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <div className="contenedor-front">
+                      <h2>BLACK MESA PREMIUM CARD</h2>
+                      <img src={chip} alt="chip" className="chip" />
+                      <p className="numero-cuenta">
+                        {tarjeta.numeroTarjeta
+                          .toString()
+                          .replace(/\B(?=(\d{4})+(?!\d))/g, " ")}
+                      </p>
+                      <div className="contenedor-fecha-nombre">
+                        <div>
+                          <p>
+                            <span>{tarjeta.fechaVencimiento}</span>
+                          </p>
+                          <h2 id="nombre-usuario">{tarjeta.nombreUsuario}</h2>
+                        </div>
+                        <img src={visa} alt="visa" className="visa" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flip-card-back">
+                    <div className="contenedor-back">
+                      <article className="contenedor-codigo-parrafo">
+                        <div className="contenedor-cvv">
+                          <div className="espacio-codigo">numb</div>
+                          <div className="cvv">
+                            <p>{tarjeta.cvv}</p>
+                          </div>
+                        </div>
+                        <p>Cupo disponible: {tarjeta.cupoTarjeta}</p>
+                      </article>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            );
+          })}
         </div>
-        <div className="informacion">
-            <p>informacion</p>
-        </div>
-    </div>
-  )
-}
+      </div>
+    )
+  );
+};
 
-export default Mistarjetas
+export default Mistarjetas;
